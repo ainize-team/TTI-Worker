de4ad87cc05ba6073f4d6349abe1eaeca08638cf
YoungJae Kim <byeongal@comcom.ai>
2022-09-06 18:40:16 +0900

    remove unused code

--- src/ml_model.py
+++ src/ml_model.py
@@ -73,16 +73,6 @@ class CustomStableDiffusionSafetyChecker(PreTrainedModel):
 
         has_nsfw_concepts = [len(res["bad_concepts"]) > 0 for res in result]
 
-        # for idx, has_nsfw_concept in enumerate(has_nsfw_concepts):
-        #     if has_nsfw_concept:
-        #         images[idx] = np.zeros(images[idx].shape)  # black image
-
-        # if any(has_nsfw_concepts):
-        #     logger.warning(
-        #         "Potential NSFW content was detected in one or more images. A black image will be returned instead."
-        #         " Try again with a different prompt and/or seed."
-        #     )
-
         return images, has_nsfw_concepts
 
 
