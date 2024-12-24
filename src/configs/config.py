07224d96e426198f922fbc80246d3aa119037d0a
DHBaek <baek@comcom.ai>
2024-12-24 10:40:20 +0900

    refactor: update default firebase app name

--- src/configs/config.py
+++ src/configs/config.py
@@ -17,7 +17,7 @@ class FirebaseSettings(BaseSettings):
     cred_path: str = "/app/key/serviceAccountKey.json"
     firebase_database_url: str
     firebase_storage_bucket: str
-    firebase_app_name: str = "text-to-art"
+    firebase_app_name: str = "text_to_art"
 
 
 celery_worker_settings = CeleryWorkerSettings()
